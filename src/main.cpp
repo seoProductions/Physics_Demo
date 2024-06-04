7c25754651d4ae164b87b984a70b63aa977c1c0a
seoProductions <duque.eliseo.7@gmail.com>
2024-06-03 23:25:15 -0500

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -13,36 +13,50 @@ int main()
 
     // good to have - variables
     const double PI = 3.1415;
-    bool dev_tools_active = false;
+    bool dev_tools_active = true;
+
+    float mainView_angle = 0;
+    float bricks_angle   = 0;
+
     std::string log = "text here";
     sf::Clock clock;
-
 
     //SFML
     sf::Texture bricks_texture;
     sf::Sprite  bricks_sprite;
 
-    bricks_sprite.setPosition(window.getSize().x / 2,
-                              window.getSize().y / 2);
-    bricks_sprite.setRotation(0.f);
-
-    //VIEWS
-    float mainView_angle = 0;
-    sf::View mainView(window.getView());
-             mainView.setCenter(mainView.getSize().x / 2,
-                                mainView.getSize().y / 2);
-             mainView.setViewport(sf::FloatRect(0.5f, 0.5f, 0.5f, 0.25f));
-
-
-    //SFML Sprite actions
+    //SFML Sprite set up
     if (!bricks_texture.loadFromFile("/home/seo/CLionProjects/physics_plane_test/bricks.jpg")) return EXIT_FAILURE;
     bricks_sprite.setTexture(bricks_texture);
     bricks_texture.setRepeated(true);
 
+    bricks_sprite.setOrigin(bricks_texture.getSize().x / 2,
+                            bricks_texture.getSize().y / 2);
+    bricks_sprite.setPosition(window.getSize().x / 2,
+                              window.getSize().y / 2);
+    bricks_sprite.setRotation(0.f);
+
+
+    //VIEWS
+    sf::View mainView(window.getView());
+             mainView.setCenter(mainView.getSize().x / 2,
+                                mainView.getSize().y / 2);
+
+    //VIEW TRANSFORM  (for dragging)
+    sf::Vector2i prev_mouse_pos (0,0);
+
+
     while (window.isOpen())
     {
 
-        //Events
+        ImGui::SFML::Update(window, clock.restart());
+
+        //SFML VIEW LOGIC
+        window.setView(mainView);
+        mainView.setRotation(mainView_angle * 180 / PI);
+        bricks_sprite.setRotation(bricks_angle * 180 / PI);
+
+        // SFML-IMGUI EVENTS
         for (auto event = sf::Event{}; window.pollEvent(event);)
         {
             ImGui::SFML::ProcessEvent(window, event);
@@ -51,10 +65,27 @@ int main()
             {
                 window.close();
             }
+            //MOUSE
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left))    log = "left click detected";
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Middle))  log = "middle click detected";
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Right))   log = "right click detected";
+
+            //if (event.mouseWheel.delta != 0)
+            //{
+            //    (event.mouseWheel.delta > 0) ? log = "scroll up detected": log = "scroll down detected";
+            //}
+            /*if (ImGui::IsMouseDragging(ImGuiMouseButton_Left)) {
+                //Drag the main view
+                log = "left click dragged";
+                mainView.move(prev_mouse_pos.x - sf::Mouse::getPosition(window).x,
+                              prev_mouse_pos.y - sf::Mouse::getPosition(window).y);
+                prev_mouse_pos = sf::Mouse::getPosition(window);
+            }*/
+
         }
-        ImGui::SFML::Update(window, clock.restart());
 
 
+        ImGui::Begin("test window", NULL, ImGuiWindowFlags_AlwaysUseWindowPadding);
         if (ImGui::BeginMainMenuBar())
         {
             if (ImGui::Button("Kinematics")) { log = "Kinematics clicked"; }
@@ -64,13 +95,15 @@ int main()
             ImGui::TextColored(ImVec4(244,488,30,255), "More");
             if (ImGui::Button("Dev-Tools"))  { dev_tools_active = !dev_tools_active; }
 
-
             ImGui::EndMainMenuBar();
         }
+        ImGui::End();
 
+
+        //Events
         if (dev_tools_active)
         {
-            ImGui::Begin("Main Window", &dev_tools_active, ImGuiWindowFlags_AlwaysAutoResize);
+            ImGui::Begin("Pre-Build Testing Window", &dev_tools_active, ImGuiWindowFlags_AlwaysUseWindowPadding);
             //menu
             if (ImGui::BeginMenuBar())
             {
@@ -81,9 +114,20 @@ int main()
                 }
                 ImGui::EndMenuBar();
             }
+
+            //DEBUG
             ImGui::SliderAngle("View Rotation", &mainView_angle);
             ImGui::Text("Main_View_Rotation");
             ImGui::Text(std::to_string(mainView_angle).c_str());
+
+            ImGui::SliderAngle("Bricks Rotation", &bricks_angle);
+            ImGui::Text("Bricks_Rotation");
+            ImGui::Text(std::to_string(bricks_angle).c_str());
+
+            ImGui::Text("Bricks_Origin");
+            ImGui::Text(std::to_string(bricks_sprite.getOrigin().x).c_str());
+            ImGui::Text(std::to_string(bricks_sprite.getOrigin().y).c_str());
+
 
             //display age
             ImGui::Text("Log INFO:");
@@ -91,11 +135,6 @@ int main()
             ImGui::End();
         }
 
-
-
-        //SFML VIEW LOGIC
-        window.setView(mainView);
-        mainView.setRotation(mainView_angle * 180 / PI);
 
 
         window.clear();
